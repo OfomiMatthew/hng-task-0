c8814208bbef32d0719bca9efbbad9efc8ce77d0
MatthewOfomi <ofomimatthew7@gmail.com>
2025-01-30 21:24:29 +0100

    url configured

--- profile_project/urls.py
+++ profile_project/urls.py
@@ -4,5 +4,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('profile_app.urls')), 
+    path('', include('profile_app.urls')), 
 ]
